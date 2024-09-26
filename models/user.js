2042d756ba2850d677f09f9a92a124eab5bc426c
Paa Kwasi Kodua <paakwasi.kodua@turntabl.io>
2024-09-26 11:07:32 +0000

    removed comment

--- models/user.js
+++ models/user.js
@@ -9,7 +9,6 @@ const UserSchema = new Schema({
     username: {
         type: String,
         required: [true, 'Username is required!']
-        // match: [/^(?=.{3,40}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
     },
     image: {
         type: String
